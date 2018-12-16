94bbda33e949d8fd97bd64e00614e6190a7518e0
Michael Perez <perez.michael.d@gmail.com>
2018-12-15 16:56:12 -0800

    Add Variables module homework assignment

---
+++ python-is-easy/variables/homework/main.py
@@ -0,0 +1,10 @@
+Title = "AnyTime (Extended Mix)"
+Album = "SPRS031"
+Artist = "Don Diablo"
+Genre = "Future House"
+ReleaseDate = "2018-08-04"
+Label = "Spinnin' Records (SPRS)"
+Country = "Netherlands"
+VideoLink = "https://youtu.be/qzNQLKgaLi0"
+DurationInSeconds = 274
+DurationInMinutes = 4.57
