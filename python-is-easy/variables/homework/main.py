16de8269acc6517413b11ad74a51eb7967d953d1
Michael Perez <perez.michael.d@gmail.com>
2018-12-15 17:23:38 -0800

    Add print statements

--- python-is-easy/variables/homework/main.py
+++ python-is-easy/variables/homework/main.py
@@ -8,3 +8,14 @@ Country = "Netherlands"
 VideoLink = "https://youtu.be/qzNQLKgaLi0"
 DurationInSeconds = 274
 DurationInMinutes = 4.57
+
+print(Title)
+print(Album)
+print(Artist)
+print(Genre)
+print(ReleaseDate)
+print(Label)
+print(Country)
+print(VideoLink)
+print(DurationInSeconds)
+print(DurationInMinutes)
