6ee0a53d813ec88d0141a7e8952953fab716dc6c
Michael Perez <perez.michael.d@gmail.com>
2018-12-18 19:45:56 -0800

    Add tests for functions module

---
+++ python-is-easy/functions/homework/test_functions.py
@@ -0,0 +1,10 @@
+from functions import *
+
+def test_artist():
+    assert artist() == "Don Diablo"
+
+def test_genre():
+    assert genre() == "Future House"
+
+def test_title():
+    assert title() == "AnyTime (Extended Mix)"
