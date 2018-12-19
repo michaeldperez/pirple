d3e3a49f233add19d782f6d3655f09cacf63c52d
Michael Perez <perez.michael.d@gmail.com>
2018-12-18 21:42:31 -0800

    Add basic loops homework - FizzBuzz (hw5)

---
+++ python-is-easy/loops/homework/fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    for num in range(1,101):
+        print(
+            {
+                num % 3 == 0 and num % 5 != 0 : "Fizz",
+                num % 5 == 0 and num % 3 != 0: "Buzz",
+                num % 5 == 0 and num % 3 == 0 : "FizzBuzz",
+            }.get(True, num)
+        )
+
+fizzbuzz()
