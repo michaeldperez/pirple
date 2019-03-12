111223676059bda45b60bc039084ed6724c5663e
Michael Perez <perez.michael.d@gmail.com>
2019-03-11 18:09:17 -0700

    Add note taking program

--- python-is-easy/io/homework/note.py
+++ python-is-easy/io/homework/note.py
@@ -1,85 +1,47 @@
 import os, re
 
-def makeOptions(options):
-    ''' Formats dictionary key/value pairs into
-        a multiline string in the format:
-        key1: value1,
-        key2: value2,
-        ...
-        keyN, valueN
-    '''
-    optionsString = ""
-    for key, value in options.items():
-        optionsString += "{}: {}\n".format(key, value)
-    return optionsString
+def take_note(file, prompt='Please enter your note: '):
+    text = input(prompt)
+    file.write('{}\n'.format(text))
+    file.close()
 
-def getOption(options):
-    ''' Displays a set of options
-        and returns the user selected option
-        (if and only if valid)
-    '''
-    askForOption = True
-    action = None
-    while(askForOption):
-        inputString = makeOptions(options)
-        action = input(inputString)
-        if action in options.keys():
-            askForOption = False
-    
-    return action
-
-def getMode(optionChosen, options):
-    ''' Matches user-selected option
-        with file I/O mode and returns that mode 
-    '''
-    instruction = options.get(optionChosen)
-    match = re.compile(r'Read|Delete|Append').match(instruction).group(0)
-
-    mode = {
-        "Read": "r",
-        "Delete": "w",
-        "Append": "a"
-    }.get(match)
-
-    return mode
-
-def readFile(filename):
-    '''Reads each line of a file
-    '''
-    file = open(filename, "r")
+def read(filename):
+    file = open(filename, 'r')
     for line in file.readlines():
         print(line)
+    file.close()
 
-def executeInstruction(filename, mode):
-    '''Takes a filename and a mode and executes
-        the file I/O for that instruction set
-    '''
-    if mode == "r":
-        readFile(filename)
-    else:
-        file = open(filename, mode)
-        text = input("Please enter your note: ")
-        file.write("{}\n".format(text))
-        file.close()
+def restart(filename):
+    file = open(filename, 'w+')
+    file.seek(0)
+    file.truncate()
+    take_note(file)
 
-def takeNote(options):
-    '''Prompts the user for a filename and reads,
-        writes, or appends to the file
-    '''
-    filename = input("Welcome to Note Taker.\nPlease enter a file name: ")
+def append(filename):
+    file = open(filename, 'a+')
+    take_note(file, 'Please enter the text you would like to append: ')
+
+def note_taker():
+    print('Welcome to Note Taker.')
+    name = input("Please enter a file name: ")
+    filename = '{}/{}'.format(os.getcwd(), name)
 
     if os.path.isfile(filename):
-        option = getOption(options)
-        mode = getMode(option, options)
-        executeInstruction(filename, mode)
+        print('What would you like to do?\n\tA) Read the file.\n\tB) Delete the file and start over.\n\tC) Append the file.')
+        option = input('Please enter either A, B, or C: ')
+        option = option.upper()
+        if option not in {'A', 'B', 'C'}:
+            print('{} is not a valid option. Goodbye.'.format(option))
+        else:
+            {
+                'A': read,
+                'B': restart,
+                'C': append
+            }.get(option)(filename)
     else:
-        executeInstruction(filename, "w")
-
-options = {
-    "A": "Read the file.",
-    "B": "Delete the file and start over.",
-    "C": "Append the file"
-}
+        print('{} does not exist in the current directory. It will be created.'.format(name))
+        file = open(filename, 'w+')
+        take_note(file)
 
 if __name__ == "__main__":
-    takeNote(options)
+    note_taker()
