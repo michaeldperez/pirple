e91a8f900d9dc81d78d7da47cbaf464862ad348a
Michael Perez <perez.michael.d@gmail.com>
2019-03-17 16:14:06 -0700

    Add ability to replace a single line of text

--- python-is-easy/io/homework/note.py
+++ python-is-easy/io/homework/note.py
@@ -21,22 +21,40 @@ def append(filename):
     file = open(filename, 'a+')
     take_note(file, 'Please enter the text you would like to append: ')
 
+def replace_line(filename):
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close()
+    number_of_lines_of_text = len(lines)
+    line_number = int(input('What line number would you like to replace?: '))
+    if line_number > number_of_lines_of_text:
+        print('Sorry, {} only has {} lines of text.'.format(os.path.basename(filename), number_of_lines_of_text))
+    else:
+        replacement_text = input('With what text would you like it to replaced?: ')
+        lines[line_number - 1] = '{}\n'.format(replacement_text)
+        file = open(filename, 'w')
+        file.writelines(lines)
+        file.close()
+
+
+
 def note_taker():
     print('Welcome to Note Taker.')
     name = input("Please enter a file name: ")
     filename = '{}/{}'.format(os.getcwd(), name)
 
     if os.path.isfile(filename):
-        print('What would you like to do?\n\tA) Read the file.\n\tB) Delete the file and start over.\n\tC) Append the file.')
-        option = input('Please enter either A, B, or C: ')
+        print('What would you like to do?\n\tA) Read the file.\n\tB) Delete the file and start over.\n\tC) Append the file.\n\tD) Replace a line.')
+        option = input('Please enter either A, B, C, or D: ')
         option = option.upper()
-        if option not in {'A', 'B', 'C'}:
+        if option not in {'A', 'B', 'C', 'D'}:
             print('{} is not a valid option. Goodbye.'.format(option))
         else:
             {
                 'A': read,
                 'B': restart,
-                'C': append
+                'C': append,
+                'D': replace_line
             }.get(option)(filename)
     else:
         print('{} does not exist in the current directory. It will be created.'.format(name))
