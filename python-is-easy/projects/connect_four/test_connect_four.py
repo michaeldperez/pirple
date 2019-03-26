70e3b37c485c0920a620a6361f881b02c04b5282
Michael Perez <perez.michael.d@gmail.com>
2019-03-25 19:16:26 -0700

    Add tests for able_to_move

--- python-is-easy/projects/connect_four/test_connect_four.py
+++ python-is-easy/projects/connect_four/test_connect_four.py
@@ -1,6 +1,6 @@
 import unittest
 
-from connect_four import toggle_player, player_icon
+from connect_four import toggle_player, player_icon, able_to_move
 
 class TestConnectFour(unittest.TestCase):
     def test_toggle_player1(self):
@@ -14,3 +14,9 @@ class TestConnectFour(unittest.TestCase):
     
     def test_player_two_icon(self):
         self.assertEqual(player_icon(2), 'O')
+    
+    def test_able_to_move(self):
+        self.assertTrue(able_to_move(['X', 'O', 'X', 'O', 'X']))
+    
+    def test_not_able_to_move(self):
+        self.assertFalse(able_to_move(['X', 'O', 'X', 'O', 'X', 'X']))
