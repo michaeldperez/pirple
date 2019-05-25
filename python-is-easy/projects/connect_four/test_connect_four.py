69fbfa69f9c76ab712dcda3132962893f8a703f8
Michael Perez <perez.michael.d@gmail.com>
2019-05-25 13:42:46 -0700

    import module using alias

--- python-is-easy/projects/connect_four/test_connect_four.py
+++ python-is-easy/projects/connect_four/test_connect_four.py
@@ -1,7 +1,7 @@
 import unittest
 
-from connect_four import toggle_player, player_icon, able_to_move, check_column
-
+#from connect_four import toggle_player, player_icon, able_to_move, check_column
+import connect_four as cf
 class TestConnectFour(unittest.TestCase):
     def setUp(self):
         self.board = [
@@ -17,28 +17,28 @@ class TestConnectFour(unittest.TestCase):
         pass
 
     def test_toggle_player1(self):
-        self.assertEqual(toggle_player(2), 1)
+        self.assertEqual(cf.toggle_player(2), 1)
     
     def test_toggle_player2(self):
-        self.assertEqual(toggle_player(1), 2)
+        self.assertEqual(cf.toggle_player(1), 2)
 
     def test_player_one_icon(self):
-        self.assertEqual(player_icon(1), 'X')
+        self.assertEqual(cf.player_icon(1), 'X')
     
     def test_player_two_icon(self):
-        self.assertEqual(player_icon(2), 'O')
+        self.assertEqual(cf.player_icon(2), 'O')
     
     def test_able_to_move(self):
-        self.assertTrue(able_to_move(self.board, 1))
+        self.assertTrue(cf.able_to_move(self.board, 1))
     
     def test_not_able_to_move(self):
-        self.assertFalse(able_to_move(self.board, 4))
+        self.assertFalse(cf.able_to_move(self.board, 4))
     
     def test_check_column_not_win(self):
-        self.assertFalse(check_column(self.board, 6, 'O'))
+        self.assertFalse(cf.check_column(self.board, 6, 'O'))
     
     def test_check_column_win(self):
-        self.assertTrue(check_column(self.board, 2, 'X'))
+        self.assertTrue(cf.check_column(self.board, 2, 'X'))
 
 if __name__ == '__main__':
     unittest.main()
