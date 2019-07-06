183fe134911bb4635d54ad978d94eebe71138171
Michael Perez <perez.michael.d@gmail.com>
2019-07-05 17:24:11 -0700

    Add tests for vehicle class

---
+++ python-is-easy/classes/homework/test_vehicle.py
@@ -0,0 +1,29 @@
+import unittest
+from vehicle import Vehicle
+
+class TestVehice(unittest.TestCase):
+    def setUp(self):
+        self.vehicle = Vehicle(
+            "Toyota",
+            "4Runner",
+            2016,
+            3)
+
+    def test_set_make(self):
+        self.vehicle.set_make('Honda')
+        self.assertEqual(self.vehicle.make, 'Honda')
+
+    def test_set_model(self):
+        self.vehicle.set_model('Accord')
+        self.assertEqual(self.vehicle.model, 'Accord')
+    
+    def test_set_year(self):
+        self.vehicle.set_year(2019)
+        self.assertEqual(self.vehicle.year, 2019)
+    
+    def test_set_weight(self):
+        self.vehicle.set_weight(2)
+        self.assertEqual(self.vehicle.weight, 2)
+
+if __name__ == '__main__':
+    unittest.main()
