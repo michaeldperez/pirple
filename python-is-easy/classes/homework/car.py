c0152711dda101fbcad47cb9ac2914e03687e36a
Michael Perez <perez.michael.d@gmail.com>
2019-07-05 18:26:51 -0700

    Add type annotations to car class

--- python-is-easy/classes/homework/car.py
+++ python-is-easy/classes/homework/car.py
@@ -1,26 +1,27 @@
+import typing
 from vehicle import Vehicle
 
 class Car(Vehicle):
     def __init__(
         self,
-        make,
-        model,
-        year,
-        weight):
+        make: str,
+        model: str,
+        year: int,
+        weight: int) -> None:
         Vehicle.__init__(self)
-        self.isDriving = False
+        self.isDriving: bool = False
     
-    def drive(self):
+    def drive(self) -> None:
         self.isDriving = True
     
-    def stop(self):
+    def stop(self) -> None:
         self.isDriving = False
         self._add_trip()
     
-    def _add_trip(self):
+    def _add_trip(self) -> None:
         self.trips_since_maintenance += 1
         self._check_if_needs_maintenance()
     
-    def _check_if_needs_maintenance(self):
+    def _check_if_needs_maintenance(self) -> None:
         if self.trips_since_maintenance > 100:
             self.needs_maintenance = True
