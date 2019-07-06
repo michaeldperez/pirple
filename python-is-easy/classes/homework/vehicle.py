0fa4aceb2347f85aa685b1e351db100d6bfb9385
Michael Perez <perez.michael.d@gmail.com>
2019-07-05 18:26:40 -0700

    Add type annotations to vehicle class

--- python-is-easy/classes/homework/vehicle.py
+++ python-is-easy/classes/homework/vehicle.py
@@ -1,12 +1,14 @@
+import typing
+
 class Vehicle(object):
     def __init__(
         self, 
-        make=None,
-        model=None, 
-        year=None,
-        weight=None,
-        needs_maintenance=False,
-        trips_since_maintenance=0):
+        make: str   = None,
+        model: str  = None, 
+        year: int   = None,
+        weight: int = None,
+        needs_maintenance: bool      = False,
+        trips_since_maintenance: int =  0) -> None:
         '''
             make:   manufacturer of vehicle
             model:  model of vehicle
@@ -23,16 +25,16 @@ class Vehicle(object):
         self.needs_maintenance       = needs_maintenance
         self.trips_since_maintenance = trips_since_maintenance
 
-    def set_make(self, new_make):
+    def set_make(self, new_make: str) -> None:
         self.make = new_make
     
-    def set_model(self, new_model):
+    def set_model(self, new_model: str) -> None:
         self.model = new_model
     
-    def set_year(self, new_year):
+    def set_year(self, new_year: int) -> None:
         self.year = new_year
     
-    def set_weight(self, new_weight):
+    def set_weight(self, new_weight: int) -> None:
         self.weight = new_weight
 
     def repair(self):
