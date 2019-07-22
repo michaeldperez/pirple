1a933a8746874a4afe3ce263277daf7f7b844459
Michael Perez <perez.michael.d@gmail.com>
2019-07-21 19:02:11 -0700

    Refactor tests for count

--- python-is-easy/importing/homework/test_itertools_demo.py
+++ python-is-easy/importing/homework/test_itertools_demo.py
@@ -1,43 +1,43 @@
 import unittest
-from itertools_demo import ItoolsConfig, Itertools
+from itertools_demo import Itertools
 
 class TestItertools(unittest.TestCase):
     def setUp(self):
-        self.config = ItoolsConfig(
-            count_start = 0,
-            count_step  = 1,
-            cycle_iter  = 'ABCD',
-            repeat_obj  = 'Hello'
-        )
-        self.iter_tools = Itertools(self.config)
+        self.itertools = Itertools()
     
     def test_counter(self):
-        self.assertEqual(next(self.iter_tools.counter), 0)
-    
-    def test_count_initial(self):
-        self.assertEqual(self.iter_tools.count(), 0)
-    
+        self.assertEqual(next(self.itertools.count()), 0)
+
     def test_count_n_times(self):
-        count_to_five = self.iter_tools.count(5)
+        counter = self.itertools.count()
+        count_to_five = None
+        for _ in range(5):
+            count_to_five = next(counter)
         self.assertEqual(count_to_five, 4)
     
-    def test_cycler(self):
-        self.assertEqual(next(self.iter_tools.cycler), 'A')
+    def test_counter_step(self):
+        counter = self.itertools.count(start=1, step=2)
+        odds = list(next(counter) for _ in range(5))
+        self.assertEqual(odds, [1,3,5,7,9])
     
-    def test_cycle_initial(self):
-        self.assertEqual(self.iter_tools.cycle(), 'A')
+    # def test_cycler(self):
+    #     self.assertEqual(next(self.iter_tools.cycler), 'A')
     
-    def test_cycle_n_times(self):
-        self.assertEqual(self.iter_tools.cycle(10), 'B')
+    # def test_cycle_initial(self):
+    #     self.assertEqual(self.iter_tools.cycle(), 'A')
     
-    def test_repeater(self):
-        self.assertEqual(next(self.iter_tools.repeater), 'Hello')
+    # def test_cycle_n_times(self):
+    #     self.assertEqual(self.iter_tools.cycle(10), 'B')
     
-    def test_repeat(self):
-        self.assertEqual(self.iter_tools.repeat(), 'Hello')
+    # def test_repeater(self):
+    #     self.assertEqual(next(self.iter_tools.repeater), 'Hello')
     
-    def test_repeat_n_times(self):
-        self.assertEqual(self.iter_tools.repeat(10), 'Hello')
+    # def test_repeat(self):
+    #     self.assertEqual(self.iter_tools.repeat(), 'Hello')
+    
+    # def test_repeat_n_times(self):
+    #     self.assertEqual(self.iter_tools.repeat(10), 'Hello')
+
 
 
 if __name__ == '__main__':
