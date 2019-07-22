b314b29f25d6f9305c7c7ebc695f02c267992f3b
Michael Perez <perez.michael.d@gmail.com>
2019-07-21 18:46:12 -0700

    Remove @property annotation

--- python-is-easy/importing/homework/itertools_demo.py
+++ python-is-easy/importing/homework/itertools_demo.py
@@ -1,14 +1,11 @@
 import itertools
 
 class Itertools(object):
-    @property
     def counter(self, start=0, step=1):
         return itertools.count(start, step)
     
-    @property
     def cycle(self, iterable):
         return itertools.cycle(iterable)
     
-    @property
     def repeat(self, object, times=None):
         return itertools.repeat(object, times)
