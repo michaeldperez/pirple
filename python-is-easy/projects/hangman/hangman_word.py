a83605f0ea0ee3d17f556c13ee2d7072a11630b3
Michael Perez <perez.michael.d@gmail.com>
2019-08-24 18:00:01 -0700

    Add methods for _obfuscate, _reveal, and _discovered

--- python-is-easy/projects/hangman/hangman_word.py
+++ python-is-easy/projects/hangman/hangman_word.py
@@ -1,12 +1,18 @@
 class HangmanWord:
     def __init__(self, word):
         self.word = word
+        self.obfuscated_word = self._obfuscate(word)
     
     def check(self, letter):
         if self._contains(self.word, letter):
-            return self._find_indices(self.word, letter)
+            indices = self._find_indices(self.word, letter)
+            self.obfuscated_word = self._reveal(letter, indices, self.obfuscated_word)
+            if self._discovered(self.word, self.obfuscated_word):
+                return (True, True, None)
+            else:
+                return (True, False, self.obfuscated_word)
         else:
-            return None
+            return (False, False, self.obfuscated_word)
     
     def _contains(self, word, letter):
         return (False, True)[letter in word]
@@ -17,3 +23,17 @@ class HangmanWord:
             if word[index] == letter:
                 indices.append(index)
         return indices
+    
+    def _obfuscate(self, word):
+        obfuscated = ""
+        for _ in range(len(word)):
+            obfuscated += "_"
+        return obfuscated
+    
+    def _reveal(self, letter, indices, obfuscated_word):
+        for index in indices:
+            obfuscated_word[index] = letter
+        return obfuscated_word
+    
+    def _discovered(self, word, obfuscated_word):
+        return word == obfuscated_word
