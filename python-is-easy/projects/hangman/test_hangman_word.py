e8422559599f46ad761518b08ccdada6ef5f9cc7
Michael Perez <perez.michael.d@gmail.com>
2019-08-24 18:00:30 -0700

    Update tests for new hangman_word implementation

--- python-is-easy/projects/hangman/test_hangman_word.py
+++ python-is-easy/projects/hangman/test_hangman_word.py
@@ -6,22 +6,51 @@ class HangmanWordTest(unittest.TestCase):
     def setUp(self):
         self.hangman_word = HangmanWord("hello")
     
-    def test_check_when_true(self):
+    def test_check_when_true_and_complete(self):
+        self.hangman_word.obfuscated_word = "hell_"
         self.hangman_word._contains = MagicMock(name="contains")
         self.hangman_word._contains.return_value = True
         self.hangman_word._find_indices = MagicMock(name="find")
         self.hangman_word._find_indices.return_value = [4]
-        indicies = self.hangman_word.check("o")
+        self.hangman_word._reveal = MagicMock(name="reveal")
+        self.hangman_word._reveal.return_value = "hello"
+        self.hangman_word._discovered = MagicMock(name="discovered")
+        self.hangman_word._discovered.return_value = True
+        letter_exists, is_discovered, obfuscated_word  = self.hangman_word.check("o")
         self.hangman_word._contains.assert_called_with("hello", "o")
         self.hangman_word._find_indices.assert_called_with("hello", "o")
-        self.assertListEqual(indicies, [4])
+        self.hangman_word._reveal.assert_called_with("o", [4], "hell_")
+        self.hangman_word._discovered.assert_called_with("hello", "hello")
+        self.assertTrue(letter_exists)
+        self.assertTrue(is_discovered)
+        self.assertIsNone(obfuscated_word)
+    
+    def test_when_true_and_incomplete(self):
+        self.hangman_word._contains = MagicMock(name="contains")
+        self.hangman_word._contains.return_value = True
+        self.hangman_word._find_indices = MagicMock(name="find")
+        self.hangman_word._find_indices.return_value = [2, 3]
+        self.hangman_word._reveal = MagicMock(name="reveal")
+        self.hangman_word._reveal.return_value = "__ll_"
+        self.hangman_word._discovered = MagicMock(name="discovered")
+        self.hangman_word._discovered.return_value = False
+        letter_exists, is_discovered, obfuscated_word  = self.hangman_word.check("l")
+        self.hangman_word._contains.assert_called_with("hello", "l")
+        self.hangman_word._find_indices.assert_called_with("hello", "l")
+        self.hangman_word._reveal.assert_called_with("l", [2,3], "_____")
+        self.hangman_word._discovered.assert_called_with("hello", "__ll_")
+        self.assertTrue(letter_exists)
+        self.assertFalse(is_discovered)
+        self.assertEqual(obfuscated_word, "__ll_")
     
     def test_check_when_false(self):
         self.hangman_word._contains = MagicMock(name="contains")
         self.hangman_word._contains.return_value = False
-        indices = self.hangman_word.check("x")
-        self.hangman_word._contains.assert_called_with("hello", "x") 
-        self.assertIsNone(indices)
+        letter_exists, is_discovered, obfuscated_word = self.hangman_word.check("x")
+        self.hangman_word._contains.assert_called_with("hello", "x")
+        self.assertFalse(letter_exists)
+        self.assertFalse(is_discovered)
+        self.assertEqual(obfuscated_word, "_____")
 
 if __name__ == "__main__":
     unittest.main()
