7b01cdc7a794d829e5943697e49a1a364cf021bc
Michael Perez <perez.michael.d@gmail.com>
2019-09-09 20:07:33 -0700

    Add request handler scaffold

--- nodejs-master-class/homework/hello-world/index.js
+++ nodejs-master-class/homework/hello-world/index.js
@@ -1,8 +1,24 @@
-const http  = require('http');
-const url   = require('url');
+const http              = require('http');
+const url               = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const requestHandler = (req, res) => {
+    let payload;
+    const parsedUrl   = url.parse(req.url, true);
+    const path        = parsedUrl.pathname;
+    const queryStr    = parsedUrl.query;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method      = req.method.toUpperCase();
+    const headers     = req.headers;
+    const decoder     = new StringDecoder('utf-8');
 
+    req.on('data', data => {
+        payload += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        payload += decoder.end();
+    });
 };
 
 const server = http.createServer(requestHandler);
