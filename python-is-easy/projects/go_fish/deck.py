aa4a41d21f0b7dd7d079cb91b892d2ed12b8358f
Michael Perez <perez.michael.d@gmail.com>
2019-09-15 14:29:26 -0700

    Add create and shuffle methods

--- python-is-easy/projects/go_fish/deck.py
+++ python-is-easy/projects/go_fish/deck.py
@@ -1,3 +1,5 @@
+import random
+
 class Deck:
     suits = [
         "\u2663", # club
@@ -5,6 +7,7 @@ class Deck:
         "\u2666", # diamond
         "\u2665"  # heart
     ]
+
     ranks = [
         'K', 'Q', 'J', '10', 
         '9', '8', '7', '6', 
@@ -13,3 +16,11 @@ class Deck:
  
     def __init__(self):
         self.cards = []
+    
+    def create(self):
+        for suit in Deck.suits:
+            for rank in Deck.ranks:
+                self.cards.append(f'{rank}{suit}')
+    
+    def shuffle(self):
+        random.shuffle(self.cards)
