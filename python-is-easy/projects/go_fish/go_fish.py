c2fecdadd094d293b7ceda6f37c0505538e69352
Michael Perez <perez.michael.d@gmail.com>
2019-09-30 00:02:52 -0700

    Move functions to dealer class

--- python-is-easy/projects/go_fish/go_fish.py
+++ python-is-easy/projects/go_fish/go_fish.py
@@ -3,13 +3,9 @@ from player import Player
 
 class GoFish:
     def __init__(self):
-        self.player = Player()
+        self.playerOne = Player()
+        self.playerTwo = Player()
         self.deck = Deck()
-        self.prepare_deck(self.deck)
-    
-    def prepare_deck(self, deck):
-        deck.create()
-        deck.shuffle()
 
     def play(self):
         pass
