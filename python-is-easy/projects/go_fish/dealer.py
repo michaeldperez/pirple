77e3c5db265d5e6f359acb1f9f3e33f59e822e2f
Michael Perez <perez.michael.d@gmail.com>
2019-11-19 18:39:19 -0800

    Add initial dealer class

--- python-is-easy/projects/go_fish/dealer.py
+++ python-is-easy/projects/go_fish/dealer.py
@@ -1,4 +1,4 @@
-import typing
+from typing import List
 from deck import Deck
 
 class Dealer:
@@ -6,9 +6,9 @@ class Dealer:
         self.deck = deck
         self.prepare_deck(self.deck)
     
-    def prepare_deck(self, deck: Deck):
+    def prepare_deck(self, deck: Deck) -> None:
         deck.create()
         deck.shuffle()
     
-    def deal_cards(self, deck: Deck, number_of_cards: int):
+    def deal_cards(self, deck: Deck, number_of_cards: int) -> List[str]:
         return deck.draw(number_of_cards)
