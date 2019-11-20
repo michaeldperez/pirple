2d2146c759a6be7f152c32242fc6eb5cd96d11ea
Michael Perez <perez.michael.d@gmail.com>
2019-11-19 18:37:48 -0800

    Update Deck tests to work with Card class

--- python-is-easy/projects/go_fish/test_deck.py
+++ python-is-easy/projects/go_fish/test_deck.py
@@ -1,22 +1,24 @@
 import unittest
+from typing import List
+from card import Card
 from deck import Deck
 
 class TestDeck(unittest.TestCase):
     def setUp(self):
-        self.new_deck = [
-            'K\u2663',  'Q\u2663', 'J\u2663', '10\u2663',
-            '9\u2663',  '8\u2663', '7\u2663',  '6\u2663',
-            '5\u2663',  '4\u2663', '3\u2663',  '2\u2663',
-            'A\u2663',  'K\u2660', 'Q\u2660',  'J\u2660',
-            '10\u2660', '9\u2660', '8\u2660',  '7\u2660',
-            '6\u2660',  '5\u2660', '4\u2660',  '3\u2660',
-            '2\u2660',  'A\u2660', 'K\u2666',  'Q\u2666', 
-            'J\u2666', '10\u2666', '9\u2666',  '8\u2666',
-            '7\u2666',  '6\u2666', '5\u2666',  '4\u2666',
-            '3\u2666',  '2\u2666', 'A\u2666',  'K\u2665',
-            'Q\u2665',  'J\u2665', '10\u2665', '9\u2665',
-            '8\u2665',  '7\u2665', '6\u2665',  '5\u2665',
-            '4\u2665',  '3\u2665', '2\u2665',  'A\u2665'
+        self.new_deck : List[Card] = [
+            Card('K', '\u2663'),  Card('Q', '\u2663'),  Card('J', '\u2663'),  Card('10', '\u2663'),
+            Card('9', '\u2663'),  Card('8', '\u2663'),  Card('7', '\u2663'),  Card('6', '\u2663'),
+            Card('5', '\u2663'),  Card('4', '\u2663'),  Card('3', '\u2663'),  Card('2', '\u2663'),
+            Card('A', '\u2663'),  Card('K', '\u2660'),  Card('Q', '\u2660'),  Card('J', '\u2660'),
+            Card('10', '\u2660'), Card('9', '\u2660'),  Card('8', '\u2660'),  Card('7', '\u2660'),
+            Card('6', '\u2660'),  Card('5', '\u2660'),  Card('4','\u2660'),   Card('3', '\u2660'),
+            Card('2', '\u2660'),  Card('A', '\u2660'),  Card('K', '\u2666'),  Card('Q', '\u2666'), 
+            Card('J', '\u2666'),  Card('10', '\u2666'), Card('9', '\u2666'),  Card('8', '\u2666'),
+            Card('7', '\u2666'),  Card('6', '\u2666'),  Card('5', '\u2666'),  Card('4', '\u2666'),
+            Card('3', '\u2666'),  Card('2', '\u2666'),  Card('A', '\u2666'),  Card('K', '\u2665'),
+            Card('Q', '\u2665'),  Card('J', '\u2665'),  Card('10', '\u2665'), Card('9', '\u2665'),
+            Card('8', '\u2665'),  Card('7', '\u2665'),  Card('6', '\u2665'),  Card('5', '\u2665'),
+            Card('4', '\u2665'),  Card('3', '\u2665'),  Card('2', '\u2665'),  Card('A', '\u2665')
         ]
         self.deck = Deck()
     
@@ -34,9 +36,9 @@ class TestDeck(unittest.TestCase):
         self.deck.create()
         hand = self.deck.draw(7)
         self.assertListEqual([
-            'A\u2665', '2\u2665', '3\u2665',
-            '4\u2665', '5\u2665', '6\u2665',
-            '7\u2665',
+            Card('A', '\u2665'), Card('2', '\u2665'), Card('3', '\u2665'),
+            Card('4', '\u2665'), Card('5', '\u2665'), Card('6', '\u2665'),
+            Card('7', '\u2665'),
         ], hand)
 
     def test_draw_negative_cards(self):
